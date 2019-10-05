cd0afa49abf3ab126153d44ffe0f4b275bc26183
camilo Albert <pepico.valncia@gmail.com>
2019-10-05 19:23:06 +0200

    acepta el reto P360 patxi

--- p362.java
+++ p362.java
@@ -1,18 +1,26 @@
 import java.util.*;
 
-public class p362 {
-	
-	public static void main(String[] args){
-		Scanner sc = new Scanner(System.in);
-		
-		int ncasos = sc.nextInt();
-		int dia;
-		int mes;
+public class P362 {
 
-		for(int i=0; i<ncasos; i++){
+		public static void main(String[] args){
+			Scanner sc = new Scanner(System.in);
 			
+			int ncasos = sc.nextInt();
+
+
+			for(int i=0; i < ncasos; i++){
+				int dia = sc.nextInt();
+				int mes = sc.nextInt();
+				
+	            if (dia==25 && mes==12){
+	                  System.out.println("SI");
+	             } else {
+	                  System.out.println("NO"); 
+	                }
 			
 		}
-
+			
 	}
+		
 }
+
